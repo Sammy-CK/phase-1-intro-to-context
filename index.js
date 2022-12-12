9e456d8176dee1a9a8f0a907828ea18d3b759e0c
Sammy-CK <sammycherono2696@gmail.com>
2022-12-12 07:36:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-// Your code here
+function createEmployeeRecord ([fName, famName, employeeTitle, payRatePerHour]){
+    const newEmployeeRecord = {
+        firstName :  fName,
+        familyName : famName,
+        title : employeeTitle,
+        payPerHour : payRatePerHour,
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+    return newEmployeeRecord;
+}
+
+function createEmployeeRecords(ArrayOfUserArrays){
+    let usersRecords = [];
+    ArrayOfUserArrays.map(userArray => {
+       usersRecords.push(createEmployeeRecord(userArray))
+    })
+    return usersRecords
+}
+
+function createTimeInEvent(employeeRecordObj, dateStamp){
+        let dateHour = dateStamp.split(' ')
+        employeeRecordObj.timeInEvents.push({
+            type : "TimeIn",
+            hour: parseInt(dateHour[1], 10),
+            date : dateHour[0]
+        })
+        return employeeRecordObj
+    }
+
+    function createTimeOutEvent(employeeRecordObj, dateStamp){
+        let dateHour = dateStamp.split(' ')
+        employeeRecordObj.timeOutEvents.push({
+            type : "TimeOut",
+            hour: parseInt(dateHour[1], 10),
+            date : dateHour[0]
+        })
+        return employeeRecordObj
+    }
+
+    function hoursWorkedOnDate(employee, findDate){
+        let timeInEvent = employee.timeInEvents.find(timeIn => {
+            return timeIn.date === findDate
+        })
+    
+        let timeOutEvent = employee.timeOutEvents.find(timeOut =>{
+            return timeOut.date === findDate
+        })
+        return (timeOutEvent.hour - timeInEvent.hour) / 100
+    }
+
+    function wagesEarnedOnDate(employee, date){
+        let hoursWageable = hoursWorkedOnDate(employee, date)
+        return +(hoursWageable * employee.payPerHour)
+    }
+
+    function allWagesFor(employee){
+        let workedDates = employee.timeInEvents.map(timeIn =>{
+            return timeIn.date
+        })
+        let paid = workedDates.reduce((accumulator, date) => {
+            return accumulator + wagesEarnedOnDate(employee, date)
+        }, 0)
+    
+        return paid
+    }
+
+    function calculatePayroll(employeeRecordsArray){
+        return employeeRecordsArray.reduce((accumulator, employee) => {
+            return accumulator + allWagesFor(employee)
+        }, 0)
+    }
